8325b2333f584ec5cc2bb75bd71488e44bc51725
GamingX OS <edmarmaranon.ph@gmail.com>
2026-04-28 11:11:01 +0800

    [Date: 2026-04-28 | Time: 11:11:01] Updated: Update.bat

--- Update.bat
+++ Update.bat
@@ -3,7 +3,7 @@ title Git Update Menu
 color 0A
 setlocal enabledelayedexpansion
 
-REM Use current folder (same folder where bat file is)
+REM Current folder
 cd /d "%~dp0"
 
 :MENU
@@ -13,14 +13,12 @@ echo         GIT UPDATE MENU
 echo dev: MasterDoom , eD
 echo ==================================
 echo.
-echo Current Folder: %cd%
-echo.
 echo [1] Check Co Dev Update
 echo [2] Update From You
 echo [0] Exit
 echo.
 
-set /p choice=Enter option number: 
+set /p choice=Enter option number:
 
 if "%choice%"=="1" goto PULL
 if "%choice%"=="2" goto PUSH
@@ -32,34 +30,15 @@ goto MENU
 
 
 :PULL
-echo.
-echo Running: git pull
 git pull
-
-if %ERRORLEVEL% neq 0 (
-    echo Error during git pull
-    pause
-    goto MENU
-)
-
-echo.
-echo Update checked successfully!
 pause
 goto MENU
 
 
 :PUSH
-echo.
-echo Running: git add .
 git add .
 
-if %ERRORLEVEL% neq 0 (
-    echo Error during git add
-    pause
-    goto MENU
-)
-
-REM Get changed files
+REM Get updated files
 set FILES=
 for /f "delims=" %%f in ('git diff --cached --name-only') do (
     if "!FILES!"=="" (
@@ -75,38 +54,22 @@ if "!FILES!"=="" (
     goto MENU
 )
 
-REM Full Date + Time (YYYY-MM-DD HH:MM:SS)
-for /f %%i in ('powershell -NoProfile -Command "Get-Date -Format ''yyyy-MM-dd HH:mm:ss''"') do set DATETIME=%%i
+REM Get Date
+for /f "delims=" %%a in ('powershell -command "Get-Date -Format yyyy-MM-dd"') do set GITDATE=%%a
 
-REM Commit message example:
-REM [2026-04-28 11:30:45] Updated: index.php, config.php
-set MSG=[!DATETIME!] Updated: !FILES!
+REM Get Time
+for /f "delims=" %%a in ('powershell -command "Get-Date -Format HH:mm:ss"') do set GITTIME=%%a
+
+REM Final commit message format
+set MSG=[Date: !GITDATE! ^| Time: !GITTIME!] Updated: !FILES!
 
 echo.
-echo Commit Message:
 echo !MSG!
 echo.
 
 git commit -m "!MSG!"
-
-if %ERRORLEVEL% neq 0 (
-    echo Error during git commit
-    pause
-    goto MENU
-)
-
-echo.
-echo Running: git push
 git push
 
-if %ERRORLEVEL% neq 0 (
-    echo Error during git push
-    pause
-    goto MENU
-)
-
-echo.
-echo All Git commands completed successfully!
 pause
 goto MENU
 
